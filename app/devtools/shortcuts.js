0ad281052984514e177dcc9edcbfcae606ddd97e
AskeLange <askekjollerlange@gmail.com>
2018-12-29 19:22:32 +0100

    Initilial setup

---
+++ app/devtools/shortcuts.js
@@ -0,0 +1,7 @@
+
+
+// Events listeners
+// Document on key down
+document.onkeydown = e => {
+  if (e.key == 'F5') location.reload ();
+};
---
+++ app/main.js
@@ -0,0 +1,12 @@
+
+
+// Imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './devtools/shortcuts';
+import './styling/base.scss';
+
+// Entry point
+const root = document.getElementById ('root');
+ReactDOM.render (<p>Hello!</p>, root);
